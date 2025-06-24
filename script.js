0953b9a467b2d35f9104b8700c9ac3771ba21cc8
majji-chidvilash-ashok-raj <chidvilashmajji@gmail.com>
2025-06-24 20:31:39 +0530

    updated version

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-document.getElementById('buttonR').addEventListener('click',function (e){
+
+ function Registered(){
+    document.getElementById('buttonR').addEventListener('click',function (e){
     e.preventDefault();
     const emailR = document.getElementById('emailR').value;
     const passR = document.getElementById('passR').value;
     const conpassR = document.getElementById('conpassR').value;
-    if(passR!==conpassR){
-        alert('please enter the details correctly');
+    if(emailR===null){
+        alert("enter your Email");
+       
     }
+     else if(passR!==conpassR||passR===null||conpassR===null){
+            alert("enter same password");
+        }
     else{
-        window.location.href = "welcome.html"
+         document.querySelector('body').classList.add("open")
     }
+ 
+
+    
+
 })
+
+   
+}
